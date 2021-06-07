face200b50e5ca6874f30871ae50508451a9307c
= <=>
2021-06-07 11:53:50 +0300

    Exercise 1

---
+++ Exercise1.py
@@ -0,0 +1,19 @@
+'''
+1. Write a Python code to find the Requests module - version, licence, copyright
+information, author, author email, document url, title and description.
+ 
+'''
+
+import requests
+
+
+print("Python Requests module - version:",requests.__version__)
+print(requests.__license__)
+print(requests.__copyright__)
+print(requests.__author__)
+print(requests.__author_email__)
+print("Document url, title, description:")
+print(requests.__url__)
+print(requests.__title__)
+print(requests.__description__)
+
